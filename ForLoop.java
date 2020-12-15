d25527b2358d29804680aa96da8acdb301d3560c
Kuba Kwiatkowski <jakub.kwiatkowski1981@gmail.com>
2020-12-15 14:09:32 +0100

    feat: I check with the if statement whether the given numbers are even (divides them with modulo%) or not and prints the result.

--- ForLoop.java
+++ ForLoop.java
@@ -1,15 +1,34 @@
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class ForLoop {
     public static void main(String[] args) {
 
+// exercise 1
+//        int[] numbers = new int[100];
+//        for (int i = 0; i < numbers.length; i++) {
+//            numbers[i] = i + 1;
+//        }
+//        for (int i = 0; i < numbers.length; i++) {
+//            System.out.println(numbers[i]);
+//        }
 
-        int[] numbers = new int[100];
-        for (int i = 0; i < numbers.length; i++) {
-            numbers[i] = i + 1;
+// exercise 2
+
+        Scanner scanner = new Scanner(System.in);
+
+        for (int i = 0; i < 3; i++) {
+            System.out.println("Podaj liczbę do sprawdzenia");
+            int tmp = scanner.nextInt();
+
+            if (tmp % 2 == 0){
+                System.out.println("Liczba " + tmp + " jest parzysta");
+            }else {
+                System.out.println("Liczba " + tmp + " jest nieparzysta");
+            }
+
         }
-        for (int i = 0; i < numbers.length; i++) {
-            System.out.println(numbers[i]);
-        }
+        scanner.close();
+
     }
 }

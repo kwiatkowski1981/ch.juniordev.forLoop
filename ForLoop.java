f6cff50893399caab30e25dfa6b15fb592420615
Kuba Kwiatkowski <jakub.kwiatkowski1981@gmail.com>
2020-12-15 13:35:51 +0100

    feat: I created a new class in which I added a basic loop that added a  number to the one-dimensional array from the array's size range and then printed them with println.

--- ForLoop.java
+++ ForLoop.java
@@ -1,9 +1,13 @@
+import java.util.Arrays;
+
 public class ForLoop {
     public static void main(String[] args) {
         int[] numbers = new int[10];
         for (int i = 0; i < 10; i++) {
-            System.out.println("i = " +i);
-
+            numbers[i] = i + 1;
+        }
+        for (int i = 0; i < 10; i++) {
+            System.out.println(numbers[i]);
         }
     }
 }

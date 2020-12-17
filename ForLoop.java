985d157b9e4a644bc1283b557f0b0c4b74e8a692
Kuba Kwiatkowski <jakub.kwiatkowski1981@gmail.com>
2020-12-17 14:14:03 +0100

    feat: I created an array of numbers, to which I rigidly assign the values I wrote out with the for each loop

--- ForLoop.java
+++ ForLoop.java
@@ -15,20 +15,29 @@ public class ForLoop {
 
 // exercise 2
 
-        Scanner scanner = new Scanner(System.in);
+//        Scanner scanner = new Scanner(System.in);
+//
+//        for (int i = 0; i < 3; i++) {
+//            System.out.println("Podaj liczbę do sprawdzenia");
+//            int tmp = scanner.nextInt();
+//
+//            if (tmp % 2 == 0){
+//                System.out.println("Liczba " + tmp + " jest parzysta");
+//            }else {
+//                System.out.println("Liczba " + tmp + " jest nieparzysta");
+//            }
+//
+//        }
+//        scanner.close();
 
-        for (int i = 0; i < 3; i++) {
-            System.out.println("Podaj liczbę do sprawdzenia");
-            int tmp = scanner.nextInt();
+// exercise 3 (for each)
 
-            if (tmp % 2 == 0){
-                System.out.println("Liczba " + tmp + " jest parzysta");
-            }else {
-                System.out.println("Liczba " + tmp + " jest nieparzysta");
-            }
+int[]  numbers = {1, 2, 3, 4, 5};
 
-        }
-        scanner.close();
+for (int element: numbers){
+    System.out.println(element);
+}
+
 
     }
 }
